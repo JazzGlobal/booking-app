e88057ba9ce817535b4f80d2d940c7fc334d2b8a
JazzGlobal <waymer147@gmail.com>
2019-11-20 21:13:19 -0500

    Boilerplate

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 var express = require('express'),
     app = express();
+
+app.get('/', (req, res) => {
+    res.redirect('/home');
+});
+
+app.get('/home', (req, res) => {
+
+});
+
+var port = 3000;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
