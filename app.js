2f6ac82e2e34d957d55029a92c436e4ed84459b2
JazzGlobal <waymer147@gmail.com>
2019-11-26 09:41:24 -0500

    Added comments describing the start and end of logic

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require('express'),
     app = express();
 
+// == App Configuration Start ==
+
 app.set('view engine', 'ejs');
+
+// == App Configuration End ==
+
+// == Production Routes Start ==
 
 app.get('/', (req, res) => {
     res.redirect('/home');
@@ -11,9 +17,15 @@ app.get('/home', (req, res) => {
     res.render('home')
 });
 
+// == Production Routes End ==
+
+// == Test Route Start ==
+
 app.get('/test', (req, res) => {
     res.send('Test text');
 });
+
+// == Test Route End ==
 
 var port = 3000;
 app.listen(port, () => {
